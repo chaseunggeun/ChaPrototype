5da0a71612be41fba90ae1b76a17817f24805b27
chaseunggeun <tmdrms0711@gmail.com>
2025-06-10 17:43:19 +0900

    Init project

---
+++ Source/ChaPrototype/ChaPrototype.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ChaPrototype.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ChaPrototype, "ChaPrototype" );
+ 
---
+++ Source/ChaPrototype/ChaPrototypeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChaPrototypeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AChaPrototypeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AChaPrototypeGameMode();
+};
+
+
+
